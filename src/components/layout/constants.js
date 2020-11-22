80814e6790201b1481e448ef945cc3ab1d635571
Camila Bernales <camilabernales09@gmail.com>
2020-11-21 21:38:13 -0300

    feature - header and .env file

---
+++ src/components/layout/constants.js
@@ -0,0 +1,22 @@
+const routesClient=[
+    {
+        name:"Tienda",
+        route:"/store",
+        className:"block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4 hover:bg-pink-400 p-2 text-lg rounded-md"
+
+    },
+    {
+        name:"Blog",
+        route:"/blog",
+        className:"block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4 hover:bg-pink-400 p-2 text-lg rounded-md"
+
+    },
+    {
+        name:"Preguntas frecuentes",
+        route:"/questions",
+        className:"block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4 hover:bg-pink-400 p-2 text-lg rounded-md"
+
+    }
+]
+
+export {routesClient}
---
+++ src/components/routes/Routes.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {  Route, Switch } from "react-router";
+import App from "../../App"
+const Routes = () => {
+    return (
+        <Switch>
+            
+        </Switch>
+    )
+}
+
+export default Routes
